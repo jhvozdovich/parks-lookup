0dac6242ba478d9dcd714742b9b62b74dd63694b
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-06-12 11:42:18 -0700

    add Swagger documentation

--- ParksLookup/Startup.cs
+++ ParksLookup/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace ParksLookup
 {
@@ -23,6 +24,10 @@ namespace ParksLookup
       services.AddDbContext<ParksLookupContext>(opt =>
       opt.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+      services.AddSwaggerGen(c =>
+      {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "Parks Lookup API", Version = "v1" });
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -37,6 +42,17 @@ namespace ParksLookup
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
       }
+
+      // Enable middleware to serve generated Swagger as a JSON endpoint.
+      app.UseSwagger();
+
+      // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+      // specifying the Swagger JSON endpoint.
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Parks Lookup V1");
+        c.RoutePrefix = string.Empty;
+      });
 
       // app.UseHttpsRedirection();
       app.UseMvc();
